485d7e3b214881c6a418556b652ba4b34c80a5cc
Sagi Kandleker <sagikandleker@gmail.com>
2018-04-22 21:04:17 +0300

    Removed

--- Thread_Watcher.py
+++
@@ -1,9 +0,0 @@
-import threading
-from watchdog.observers import Observer
-from watchdog.events import FileSystemEventHandler
-
-
-class MyHandler(FileSystemEventHandler):
-    
-    def on_modified(self, event):
-        print "Be careful! Your folder is Infected"
